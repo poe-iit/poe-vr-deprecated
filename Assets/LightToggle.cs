5a609acff122c8daa8b0a276b8ea6a7fa477ccd0
Jacob <jakolker2@gmail.com>
2019-11-22 10:08:28 -0600

    add partial work on light script

---
+++ Assets/LightToggle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightToggle : MonoBehaviour
+{
+    //public GameObject[] Lights;
+    public Light[] Lights;
+    public float timeToWait = 5;
+    public float timeUsed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeUsed = timeToWait;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        for (int i = 0; i < Lights.Length;)
+        {
+            timeUsed -= Time.deltaTime;
+            Debug.Log(timeUsed);
+            if (timeUsed>=timeToWait) {
+                Lights[i].enabled = !Lights[i].enabled;
+                i++;
+                timeUsed=timeToWait;
+            }
+        }
+    }
+}
