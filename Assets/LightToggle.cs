7d986f1c4449f0edcd1e6099a8a6ed67d8f76de0
Jacob <jakolker2@gmail.com>
2019-11-22 13:02:53 -0600

    add working light toggle script

--- Assets/LightToggle.cs
+++ Assets/LightToggle.cs
@@ -4,29 +4,80 @@ using UnityEngine;
 
 public class LightToggle : MonoBehaviour
 {
-    //public GameObject[] Lights;
     public Light[] Lights;
-    public float timeToWait = 5;
+
     public float timeUsed;
+    public bool toggleLights = false;
+    public int i = 0;
+    public float targetTime = 1.0f;
+    public Color color0 = Color.red;
+    //public Color color1 = Color.blue;
+    public Color initialColor = Color.white;
+    bool colorChanged = false;
+    //public int lastIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
-        timeUsed = timeToWait;
+        timeUsed = targetTime;
+        for (int count = 0; count < Lights.Length; count++)
+        {
+            Lights[count].color = initialColor;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        for (int i = 0; i < Lights.Length;)
+        if (toggleLights == false)
         {
-            timeUsed -= Time.deltaTime;
-            Debug.Log(timeUsed);
-            if (timeUsed>=timeToWait) {
-                Lights[i].enabled = !Lights[i].enabled;
-                i++;
-                timeUsed=timeToWait;
+            for (int count = 0; count < Lights.Length; count++)
+            {
+                Lights[count].color = initialColor;
             }
+            colorChanged = false;
+        }
+        if (toggleLights)
+        {
+            if (colorChanged == false) { }
+            for (int count = 0; count < Lights.Length; count++)
+            {
+                Lights[count].color = color0;
+
+            }
+            colorChanged = true;
+
+            timeUsed -= Time.deltaTime;
+
+            if (timeUsed <= 0.0f)
+            {
+                if (i != 0)
+                {
+                    Lights[i - 1].enabled = !Lights[i - 1].enabled;
+                }
+                if (i >= Lights.Length)
+                {
+
+                    i = 0;
+                    Lights[i].enabled = !Lights[i].enabled;
+                    timeUsed = targetTime;
+                    i++;
+                    Debug.Log("reached end of array");
+
+                }
+                else
+                {
+                    Lights[i].enabled = !Lights[i].enabled;
+                    //lastIndex = i;
+                    i++;
+                    timeUsed = targetTime;
+                }
+
+            }
+
+
+
+
         }
     }
+
 }
