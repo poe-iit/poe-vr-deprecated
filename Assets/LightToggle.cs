2dd98f384a5be1e27b77abc47c45111c7c48908d
Jacob <jakolker2@gmail.com>
2019-11-23 23:55:46 -0600

    Add working light path system

--- Assets/LightToggle.cs
+++ Assets/LightToggle.cs
@@ -13,70 +13,128 @@ public class LightToggle : MonoBehaviour
     public Color color0 = Color.red;
     //public Color color1 = Color.blue;
     public Color initialColor = Color.white;
+    public Color colorPath = Color.blue;
     bool colorChanged = false;
+    public bool enableButtons = false;
+    public bool allowedToRun = false;
+    public Light[] buttonLights;
+    public GameObject[] allCeilingLightsGameobject;
+    public static int numberLights = 0;
+    
     //public int lastIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
         timeUsed = targetTime;
-        for (int count = 0; count < Lights.Length; count++)
+        /*for (int count = 0; count < Lights.Length; count++)
         {
             Lights[count].color = initialColor;
+        }*/
+        allCeilingLightsGameobject = GameObject.FindGameObjectsWithTag("ceiling_light");
+        for (int i = 0; i<allCeilingLightsGameobject.Length; i++)
+        {
+            allCeilingLightsGameobject[i].GetComponent<Light>().color=initialColor;
         }
+        //numberLights = allCeilingLightsGameobject.Length;
+        //allCeilingLights[0] = allCeilingLightsGameobject[0].GetComponent<Light>();
+        /*public Light[] allCeilingLights = new Light[numberLights];
+        for (int i=0; i<allCeilingLightsGameobject.Length; i++) {
+            allCeilingLights[i] = allCeilingLightsGameobject[i].GetComponent<Light>();
+            allCeilingLights[i].color = initialColor;
+        }*/
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (toggleLights == false)
+       
+        if (toggleLights == false && colorChanged==false)
         {
-            for (int count = 0; count < Lights.Length; count++)
+            for (int count = 0; count < allCeilingLightsGameobject.Length; count++)
             {
-                Lights[count].color = initialColor;
-            }
-            colorChanged = false;
-        }
-        if (toggleLights)
-        {
-            if (colorChanged == false) { }
-            for (int count = 0; count < Lights.Length; count++)
-            {
-                Lights[count].color = color0;
-
+                //Lights[count].color = initialColor;
+                allCeilingLightsGameobject[count].GetComponent<Light>().color = initialColor;
+                //allCeilingLights.GetComponent<light>().color= initialColor;
+                //allCeilingLights[0].GetComponent<lights>()
+                
             }
             colorChanged = true;
-
-            timeUsed -= Time.deltaTime;
-
-            if (timeUsed <= 0.0f)
+        }
+        
+            if (allowedToRun)
             {
-                if (i != 0)
+            Debug.Log("allowed to run");
+            if (colorChanged == false) { }
+                for (int count = 0; count < allCeilingLightsGameobject.Length; count++)
                 {
-                    Lights[i - 1].enabled = !Lights[i - 1].enabled;
-                }
-                if (i >= Lights.Length)
-                {
-
-                    i = 0;
-                    Lights[i].enabled = !Lights[i].enabled;
-                    timeUsed = targetTime;
-                    i++;
-                    Debug.Log("reached end of array");
+                    //Lights[count].color = color0;
+                    allCeilingLightsGameobject[count].GetComponent<Light>().color = color0;
 
                 }
-                else
+                colorChanged = true;
+
+                timeUsed -= Time.deltaTime;
+
+                if (timeUsed <= 0.0f)
                 {
-                    Lights[i].enabled = !Lights[i].enabled;
-                    //lastIndex = i;
-                    i++;
-                    timeUsed = targetTime;
+                    if (i != 0)
+                    {
+                        Lights[i - 1].enabled = !Lights[i - 1].enabled;
+                        //Lights[i-1].color = color0;
+                    }
+                    if (i >= Lights.Length)
+                    {
+
+                        i = 0;
+                        Lights[i].enabled = !Lights[i].enabled;
+                        //Lights[i].color = color0;
+                        timeUsed = targetTime;
+                        i++;
+                        Debug.Log("reached end of array");
+
+                    }
+                    else
+                    {
+                        Lights[i].enabled = !Lights[i].enabled;
+                        //Lights[i].color = colorPath;
+                        //lastIndex = i;
+                        i++;
+                        timeUsed = targetTime;
+                    }
+
                 }
+
+
+
 
             }
+        
+    }
+    public void toggleTheLights ()
+    {
+        //toggleLights = !toggleLights;
+        //enableButtons = false;
+        buttonLights[0].enabled = false;
+        buttonLights[1].enabled = false;
+        if (enableButtons) {
+            allowedToRun = !allowedToRun;
+            //buttonLights[0].enabled = false;
+            //buttonLights[1].enabled = false;
+            enableButtons = false;
+            colorChanged = false;
+        }
+    }
 
-
-
-
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.gameObject.tag);
+        if (other.gameObject.tag=="id_card")
+        {
+            Debug.Log("Got ID card");
+            enableButtons = true;
+            buttonLights[0].enabled = true;
+            buttonLights[1].enabled = true;
         }
     }
 
